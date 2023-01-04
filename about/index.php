434d2ca49683e4900c3495e614cd6c008599a76a
eferreyro <emafe7472@gmail.com>
2023-01-03 23:31:20 -0600

    primer commit
    Agregando cambios

---
+++ about/index.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:title" content="Emmanuel Ferreyro" />
+    <meta property="og:description" content="Pagina personal de Emmanuel Ferreyro." />
+    <meta property="og:image" content="https://emmafe.com/img/EF-LOGO2.png" />
+    <title>About Emmanuel Ferreyro</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+</head>
+
+<body>
+    <div class="container">
+        <div class="lSide">
+            <div class="profileText">
+                <div class="imgBox">
+                    <img src="assets/img/emmaFront.PNG" alt="">
+                </div>
+                <h2>Emmanuel Ferreyro<br><span>System Engineer & Web Developer</span></h2>
+            </div>
+            <div class="contactInfo">
+
+                <h3 class="title">Contact Info</h3>
+                <ul class="list-group">
+
+                    <li class="list-group-item active">
+                        <span class="icon"><i class="fab fa-whatsapp"></i></i>
+                        </span>
+                        <span class="text"><a href="https://api.whatsapp.com/send?phone=+525576691833&text=Hola%20Emmanuel!%20he%20llegado%20a%20ti%20por%20medio%20de%20tu%20pagina%20web%20y%20me%20gustaria%20ponerme%20en%20contacto%20contigo.">+54 442 853 6835</a></span>
+                    </li>
+                    <li class="list-group-item active">
+                        <span class="icon"><i class="fas fa-envelope-open-text"></i></span>
+                        <span class="text"><a href="mailto:emafe7472@gmail.com?subject=Hi%20Emmanuel%20Ferreyro&body=Escriba%20aqui%20su%20mensaje">emafe7472@gmail.com</a></span>
+                    </li>
+                    <li class="list-group-item active">
+                        <span class="icon"><i class="fa fa-globe" aria-hidden="true"></i></span>
+                        <span class="text"><a href="https://emmafe.com">www.emmafe.com</a></span>
+                    </li>
+                    <li class="list-group-item active">
+                        <span class="icon"><i class="fab fa-linkedin"></i></span>
+                        <span class="text"><a href="https://www.linkedin.com/in/eferreyro/">LinkedIn.com/eferreyro</a></span>
+                    </li>
+                    <li class="list-group-item active">
+                        <span class="icon"><i class="fab fa-github"></i></span>
+                        <span class="text"><a href="https://github.com/eferreyro">GitHub.com/eferreyro</a></span>
+                    </li>
+                    <li class="list-group-item active">
+                        <span class="icon"><i class="fa fa-map-marker"></i></span>
+                        <span class="text"><a href="https://www.google.com/maps/place/Santiago+de+Quer%C3%A9taro,+Qro./@20.6122138,-100.4452366,13z/data=!3m1!4b1!4m5!3m4!1s0x85d35b8fdc5b9255:0x97b094aa561b832f!8m2!3d20.5887596!4d-100.3898907">Queretaro, Mexico</a></span>
+                    </li>
+                </ul>
+
+            </div>
+            <div class="contactInfo educationInfo">
+                <h3 class="title">Education Info</h3>
+                <ul class="list-group">
+                    <li class="list-group-item active">
+                        <h5>2004 ~ 2009</h5>
+                        <h4>Systems Engineer</h4>
+                        <h4>Mendoza University</h4>
+                    </li>
+                    <li class="list-group-item active">
+                        <h5>1998 ~ 2003</h5>
+                        <h4>Tecnico superior en analisis y programacion de sistemas</h4>
+                        <h4>9-012 IES en Informatica</h4>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="contactInfo langInfo">
+                <h3 class="title">Languages</h3>
+                <ul class="list-group">
+                    <li class="list-group-item active">
+                        <span class="text">Español</span>
+                        <span class="percent">
+                            <div style="width: 90%"></div>
+                        </span>
+                    </li>
+                    <li class="list-group-item active">
+                        <span class="text">Ingles</span>
+                        <span class="percent">
+                            <div style="width: 60%"></div>
+                        </span>
+                    </li>
+                    <li class="list-group-item active">
+                        <span class="text">Frances</span>
+                        <span class="percent">
+                            <div style="width: 45%"></div>
+                        </span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="rSide">
+            <div class="profile">
+
+                <h2 class="title2">Profile</h2>
+                <p>Mi perfil es Técnico, electrónico y analista de sistemas con experiencia en el rubro de entretenimientos,
+                    conocimientos en sistemas Cashless, TiTo y MiCo. <br><br>
+                <p>Tengo aptitudes de liderazgo de equipos de trabajo de alto desempeño, buscando siempre la eficiencia y el mejor desempeño laboral.
+                <p> Entre mis habilidades profesionales, puedo nombrar el armado y reparación de maquinas tragamonedas de las principales marcas del mercado, estructuras de redes, administración de
+                    IT y sistemas de seguridad de la información, con sólidos conocimientos en Electrónica y Programación.<br><br>
+                    Actualmente me encuentro en la formacion de proyectos de servidores de alta disponibilidad y aprendiendo
+                    nuevas habilidades, con el uso de Frameworks como Laravel, Vue y React</p>
+
+            </div>
+
+            <div class="profile">
+                <h2 class="title2">Experience</h2>
+                <hr>
+                <div class="box">
+                    <div class="year_company">
+                        <h5>2019 ~ Present</h5>
+                        <h5>WinSystems</h5>
+                    </div>
+                    <div class="text">
+                        <h4>System Engyneer</h4>
+                        <p>Actualmente trabajando en proyectos de servidores y brindando el mejor servicio de una enorme y muy gratificante empresa. En constante aprendizaje y crecimiento con sistemas Windows Server y SQL Server.</p>
+                    </div>
+                </div>
+                <hr>
+                <div class="box">
+                    <div class="year_company">
+                        <h5>2013 ~ 2019</h5>
+                        <h5>Casino Central</h5>
+                    </div>
+                    <div class="text">
+                        <h4>IT System Manager</h4>
+                        <p>Trabaje como Gerente de Sistemas y lider de un excelente equipo de trabajo con 7 casinos en todo Mexico. El logro que mas me enorgullece de esta epoca laboral y profesional fue haber logrado una meta constante
+                            y casi inalterable de mantener menos del 3% de un parque de casi 3000 maquinas tragamonedas Fuera de Servicio. Ademas, la empresa me permitio crecer internamente, hasta lograr formar un equipo solido de Tecnicos altamente capacitados.
+                        </p>
+                    </div>
+                </div>
+                <hr>
+                <div class="box">
+                    <div class="year_company">
+                        <h5>2003 ~ 2012</h5>
+                        <h5>Teceng Gaming</h5>
+                    </div>
+                    <div class="text">
+                        <h4>Team Leader & Install Manager</h4>
+                        <p>Me especialice en la instalacion y participe en muchos proyectos de migracion e instalacion de sistemas de maquinas tragamonedas de Argentina y paises limitrofes.</p>
+                    </div>
+                </div>
+                <hr>
+
+            </div>
+            <div class="about skills">
+                <h2 class="title2">Professional Skills</h2>
+                <div class="box">
+                    <h4>CSS</h4>
+                    <div class="percent">
+                        <div style="width: 63%;"></div>
+                    </div>
+                </div>
+                <div class="box">
+                    <h4>HTML</h4>
+                    <div class="percent">
+                        <div style="width: 75%;"></div>
+                    </div>
+                </div>
+                <div class="box">
+                    <h4>JavaScript</h4>
+                    <div class="percent">
+                        <div style="width: 70%;"></div>
+                    </div>
+                </div>
+                <div class="box">
+                    <h4>Laravel</h4>
+                    <div class="percent">
+                        <div style="width: 63%;"></div>
+                    </div>
+                </div>
+                <div class="box">
+                    <h4>PHP</h4>
+                    <div class="percent">
+                        <div style="width: 65%;"></div>
+                    </div>
+                </div>
+                <div class="box">
+                    <h4>Python</h4>
+                    <div class="percent">
+                        <div style="width: 45%;"></div>
+                    </div>
+                </div>
+                <div class="box">
+                    <h4>React</h4>
+                    <div class="percent">
+                        <div style="width: 50%;"></div>
+                    </div>
+                </div>
+                <div class="box">
+                    <h4>TypeScript</h4>
+                    <div class="percent">
+                        <div style="width: 35%;"></div>
+                    </div>
+                </div>
+                <hr>
+                <br>
+
+
+                <div class="box">
+                    <h4>Linux</h4>
+                    <div class="percent">
+                        <div style="width: 55%;"></div>
+                    </div>
+                </div>
+                <div class="box">
+                    <h4>Windows Server</h4>
+                    <div class="percent">
+                        <div style="width: 65%;"></div>
+                    </div>
+                </div>
+                <hr>
+                <br>
+
+
+                <div class="box">
+                    <h4>MariaDB</h4>
+                    <div class="percent">
+                        <div style="width: 82%;"></div>
+                    </div>
+                </div>
+                <div class="box">
+                    <h4>MySQL</h4>
+                    <div class="percent">
+                        <div style="width: 82%;"></div>
+                    </div>
+                </div>
+                <div class="box">
+                    <h4>MongoDB</h4>
+                    <div class="percent">
+                        <div style="width: 35%;"></div>
+                    </div>
+                </div>
+                <div class="box">
+                    <h4>PostgreSQL</h4>
+                    <div class="percent">
+                        <div style="width: 45%;"></div>
+                    </div>
+                </div>
+                <div class="box">
+                    <h4>SQL Server</h4>
+                    <div class="percent">
+                        <div style="width: 65%;"></div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="about interest">
+                <h2 class="title2">Interest</h2>
+
+                <ul>
+                    <li><i class="fa fa-gamepad" aria-hidden="true"></i> Gaming</li>
+                    <li><i class="fa fa-coffee" aria-hidden="true"></i> Cofee</li>
+                    <li><i class="fa fa-book" aria-hidden="true"></i> Reading</li>
+                    <li><i class="fa fa-hand-spock-o" aria-hidden="true"></i> Rugby</li>
+                    <li><i class="fa fa-plane" aria-hidden="true"></i> Travel</li>
+                    <li><i class="fa fa-motorcycle" aria-hidden="true"></i> Motocross</li>
+                </ul>
+            </div>
+        </div>
+
+
+    </div>
+    </div>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,132 @@
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Emmanuel Ferreyro</title>
+  <meta name="description" content="Web personal de Emmanuel">
+  <meta property="og:title" content="Emmanuel Ferreyro" />
+  <meta property="og:url" content="https://emmafe.com" />
+  <meta property="og:site_name" content="Emmanuel Ferreyro" />
+  <meta property="og:description" content="Pagina personal de Emmanuel Ferreyro." />
+  <meta property="og:image:secure_url" content="https://emmafe.com/img/logo-emmafe.png" />
+  <meta property="og:image" content="http://emmafe.com/img/logo-emmafe200.png" />
+  <meta property="og:image:type" content="image/png" />
+  <meta property="og:type" content="article" />
+  <meta property="og:locale" content="es_MX" />
+  <meta property="og:locale:alternate" content="fr_FR" />
+  <meta property="og:locale:alternate" content="en_US" />
+  <!-- MDB icon -->
+  <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <!-- Material Design Bootstrap -->
+  <link rel="stylesheet" href="css/mdb.min.css">
+  <!-- Your custom styles (optional) -->
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<!--Navbar -->
+<nav class="mb-1 navbar navbar-expand-lg navbar-dark special-color-dark">
+  <!-- <a class="navbar-brand" href="#">LOGO</a> -->
+  <img class="img-fluid logo" src="./img/EF-LOGO2.png" alt="">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
+    aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
+    <ul class="navbar-nav mr-auto">
+    </ul>
+    <ul class="navbar-nav ml-auto nav-flex-icons">
+
+          <li class="nav-item fa-lg">
+        <a class="nav-link waves-effect waves-light" href="https://api.whatsapp.com/send?phone=+524428536835&text=Hola%20Emmanuel!%20he%20llegado%20a%20ti%20por%20medio%20de%20tu%20pagina%20web%20y%20me%20gustaria%20ponerme%20en%20contacto%20contigo.">
+          <i class="fab fa-whatsapp"></i>
+        </a>
+      </li>
+            <li class="nav-item fa-lg">
+        <a class="nav-link waves-effect waves-light" href="mailto:emafe7472@gmail.com?subject=Hi%20Emmanuel%20Ferreyro&body=Escriba%20aqui%20su%20mensaje">
+          <i class="fas fa-envelope-open-text"></i>
+        </a>
+      </li>
+      <li class="nav-item fa-lg">
+        <a class="nav-link waves-effect waves-light" href="https://github.com/eferreyro">
+          <i class="fab fa-github"></i>
+        </a>
+      </li>
+       <li class="nav-item fa-lg">
+        <a class="nav-link waves-effect waves-light" href="https://www.linkedin.com/in/eferreyro/">
+          <i class="fab fa-linkedin"></i>
+        </a>
+      </li>
+     <!-- <li class="nav-item fa-lg">
+        <a class="nav-link waves-effect waves-light" href="https://twitter.com/emmafe1983">
+          <i class="fab fa-twitter"></i>
+        </a>
+      </li> 
+      <li class="nav-item fa-lg">
+        <a class="nav-link waves-effect waves-light" href="https://eferreyro.slack.com">
+          <i class="fab fa-slack"></i>
+        </a>
+      </li>-->
+      <li class="nav-item fa-lg">
+        <a class="nav-link waves-effect waves-light" href="mailto:emafe7472@gmail.com">
+          <i class="fab fa-google-plus-g"></i>
+        </a>
+      </li>
+
+    </ul>
+  </div>
+</nav>
+<div class="banner-area"></div>
+<!--/.Navbar -->
+<!--First slide-->
+<div class="carousel-item active">
+  <!--Mask-->
+  <div class="view">
+    <!--Video source-->
+    <video muted autoplay loop playsinline>
+      <source src="https://mdbootstrap.com/img/video/Lines.mp4" type="video/mp4" />
+    </video>
+    <!-- Carousel content -->
+    <div class="full-bg-img flex-center mask rgba-indigo-light white-text">
+      <ul class="animated fadeInUp col-md-12 list-unstyled list-inline">
+        <li>
+          <h1 class="font-weight-bold text-uppercase">Emmanuel Ferreyro</h1>
+        </li>
+        <li>
+          <p class="font-weight-bold text-uppercase py-4">System Engineer & Web Developer</p>
+        </li>
+
+        <li class="list-inline-item">
+          <a href="http://about.emmafe.com"
+            class="btn btn-cyan btn-lg btn-rounded ml-0">Sobre mi</a>
+        </li>
+         <li class="list-inline-item">
+          <a href="http://about.emmafe.com"
+            class="btn btn-cyan btn-lg btn-rounded ml-0">Portafolio</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <!--/Mask-->
+</div>
+<!--/First slide-->
+  <!-- jQuery -->
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="js/mdb.min.js"></script>
+  <!-- Your custom scripts (optional) -->
+  <script type="text/javascript" src="js/sticky.js"></script>
+
+</body>
+</html>
